ac9c5c2800c4b998fe6ddfa2be17aa5f81a3cf88
Max <zimmermaxwell@gmail.com>
2025-01-15 17:11:27 -0600

    test

--- client/app-web/src/App.js
+++ client/app-web/src/App.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import { LandingPage } from './pages';
-import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import nexusTheme from './styles/theme';
+import React from "react";
+import { LandingPage } from "./pages";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import nexusTheme from "./styles/theme";
 
 const App = () => {
-
   return (
     <ThemeProvider theme={nexusTheme}>
       <CssBaseline />
-      {/* Your application components go here */}
+      {/* Your application components go comment comment */}
       <div>
-      <LandingPage/>
-    </div>
+        <LandingPage />
+      </div>
     </ThemeProvider>
   );
 };
